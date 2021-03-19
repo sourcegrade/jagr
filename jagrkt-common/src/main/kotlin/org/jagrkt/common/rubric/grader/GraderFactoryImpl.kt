7d427212d20727dc890c6369fa98bdc56a12fe62
Alexander Staeding <alexanderstaeding@gmail.com>
2021-03-19 09:43:19 +0100

    License with LGPL-3.0

--- jagrkt-common/src/main/kotlin/org/jagrkt/common/rubric/grader/GraderFactoryImpl.kt
+++ jagrkt-common/src/main/kotlin/org/jagrkt/common/rubric/grader/GraderFactoryImpl.kt
@@ -1,3 +1,22 @@
+/*
+ *   JagrKt - JagrKt.org
+ *   Copyright (C) 2021 Alexander Staeding
+ *   Copyright (C) 2021 Contributors
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package org.jagrkt.common.rubric.grader
 
 import org.jagrkt.api.rubric.Grader
