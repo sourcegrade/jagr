773697bb31edef31aaf3abda87fbb26ed60982c0
Alexander Staeding <alexanderstaeding@gmail.com>
2021-04-17 22:01:37 +0200

    Added missing JUnit dependency in api

--- jagrkt-api/build.gradle.kts
+++ jagrkt-api/build.gradle.kts
@@ -8,7 +8,9 @@ repositories {
 dependencies {
   api("com.google.inject:guice:5.0.1")
   api("org.slf4j:slf4j-api:1.7.30")
-  api("org.junit.jupiter:junit-jupiter-engine:5.7.1")
+  val junitVersion = "5.7.1"
+  api("org.junit.jupiter:junit-jupiter:$junitVersion")
+  api("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
   api("org.junit.platform:junit-platform-launcher:1.7.1")
   implementation("org.jetbrains:annotations:20.1.0")
 }
