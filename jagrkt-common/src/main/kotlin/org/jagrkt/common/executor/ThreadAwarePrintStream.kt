d2c7047ad76295031c0c00a8efeb785689f1c93d
Jan Bormet <jan.bormet@bormet.info>
2021-05-05 19:18:11 +0200

    Make the ThreadAwarePrintStream run faster (maybe) to avoid artifacts

--- jagrkt-common/src/main/kotlin/org/jagrkt/common/executor/ThreadAwarePrintStream.kt
+++ jagrkt-common/src/main/kotlin/org/jagrkt/common/executor/ThreadAwarePrintStream.kt
@@ -2,15 +2,36 @@ package org.jagrkt.common.executor
 
 import java.io.OutputStream
 import java.io.PrintStream
+import java.util.*
 
 const val GRADING_THREAD_PREFIX = "G_Thread-"
 
-class ThreadAwarePrintStream(delegate: OutputStream) : PrintStream(ThreadAwareOutputStream(delegate))
+class ThreadAwarePrintStream(delegate: OutputStream) : PrintStream(delegate) {
+  private inline fun <T> check(default: T, block: () -> T): T =
+    if (!Thread.currentThread().name.startsWith(GRADING_THREAD_PREFIX)) block() else default
 
-class ThreadAwareOutputStream(private val delegate: OutputStream) : OutputStream() {
-  override fun write(b: Int) {
-    if (!Thread.currentThread().name.startsWith(GRADING_THREAD_PREFIX)) {
-      delegate.write(b)
-    }
-  }
+  override fun print(b: Boolean) = check(Unit) { super.print(b) }
+  override fun print(c: Char) = check(Unit) { super.print(c) }
+  override fun print(i: Int) = check(Unit) { super.print(i) }
+  override fun print(l: Long) = check(Unit) { super.print(l) }
+  override fun print(f: Float) = check(Unit) { super.print(f) }
+  override fun print(d: Double) = check(Unit) { super.print(d) }
+  override fun print(s: CharArray) = check(Unit) { super.print(s) }
+  override fun print(s: String?) = check(Unit) { super.print(s) }
+  override fun print(obj: Any?) = check(Unit) { super.print(obj) }
+  override fun println() = check(Unit) { super.println() }
+  override fun println(x: Boolean) = check(Unit) { super.println(x) }
+  override fun println(x: Char) = check(Unit) { super.println(x) }
+  override fun println(x: Int) = check(Unit) { super.println(x) }
+  override fun println(x: Long) = check(Unit) { super.println(x) }
+  override fun println(x: Float) = check(Unit) { super.println(x) }
+  override fun println(x: Double) = check(Unit) { super.println(x) }
+  override fun println(x: CharArray) = check(Unit) { super.println(x) }
+  override fun println(x: String?) = check(Unit) { super.println(x) }
+  override fun println(x: Any?) = check(Unit) { super.println(x) }
+  override fun printf(format: String, vararg args: Any?): PrintStream =
+    check(this) { super.printf(format, *args) }
+
+  override fun printf(l: Locale?, format: String, vararg args: Any?): PrintStream =
+    check(this) { super.printf(l, format, *args) }
 }
