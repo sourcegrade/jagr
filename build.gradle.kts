8baa158900aa3d08e5af664746db7aaea1f2cf94
Alexander Staeding <alexanderstaeding@gmail.com>
2021-05-11 16:39:20 +0200

    Bump to Java 11 minimum

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,12 +23,12 @@ allprojects {
 
   tasks {
     withType<KotlinCompile> {
-      kotlinOptions.jvmTarget = "1.8"
+      kotlinOptions.jvmTarget = "11"
     }
     withType<JavaCompile> {
       options.encoding = "UTF-8"
-      sourceCompatibility = "1.8"
-      targetCompatibility = "1.8"
+      sourceCompatibility = "11"
+      targetCompatibility = "11"
     }
   }
 }
