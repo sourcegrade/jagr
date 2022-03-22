26e3a49a6c4f8fa647a01c4dd1b83bedb4b6732c
Alexander Staeding <alexanderstaeding@gmail.com>
2022-03-22 19:38:32 +0100

    Update copyright year to 2022 and fix some missing headers

--- core/src/main/kotlin/org/sourcegrade/jagr/core/executor/GradingQueueImpl.kt
+++ core/src/main/kotlin/org/sourcegrade/jagr/core/executor/GradingQueueImpl.kt
@@ -1,7 +1,7 @@
 /*
  *   Jagr - SourceGrade.org
- *   Copyright (C) 2021 Alexander Staeding
- *   Copyright (C) 2021 Contributors
+ *   Copyright (C) 2021-2022 Alexander Staeding
+ *   Copyright (C) 2021-2022 Contributors
  *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU Affero General Public License as published by
