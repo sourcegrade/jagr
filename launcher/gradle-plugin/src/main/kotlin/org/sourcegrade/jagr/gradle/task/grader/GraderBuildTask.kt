f8ba395530c54961e7dfa0ad8b0a0ea67011920c
Alexander Staeding <alexanderstaeding@gmail.com>
2022-09-25 22:26:15 +0200

    Fix grader build task

--- launcher/gradle-plugin/src/main/kotlin/org/sourcegrade/jagr/gradle/task/grader/GraderBuildTask.kt
+++ launcher/gradle-plugin/src/main/kotlin/org/sourcegrade/jagr/gradle/task/grader/GraderBuildTask.kt
@@ -1,24 +1,29 @@
 package org.sourcegrade.jagr.gradle.task.grader
 
 import org.gradle.api.Project
+import org.gradle.api.provider.Property
 import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.SourceSetContainer
 import org.gradle.jvm.tasks.Jar
 import org.gradle.kotlin.dsl.get
 import org.gradle.kotlin.dsl.getByType
+import org.gradle.kotlin.dsl.property
 import org.sourcegrade.jagr.gradle.GraderConfiguration
+import java.io.File
 
 @Suppress("LeakingThis")
 abstract class GraderBuildTask : Jar(), GraderTask {
 
     @get:InputFile
-    val graderInfoFile = project.buildDir.resolve("resources/jagr/grader-info.json")
+    val graderInfoFile: Property<File> = project.objects.property<File>()
+        .value(configurationName.map { project.buildDir.resolve("resources/jagr/$it/grader-info.json") })
 
     init {
         dependsOn(configurationName.map(GraderWriteInfoTask.Factory::determineTaskName))
         archiveFileName.set(graderName.map { "$it-${project.version}.jar" })
         from(graderInfoFile)
         val sourceSets = project.extensions.getByType<SourceSetContainer>()
+        from(solutionConfigurationName.map { sourceSets[it].allSource })
         from(
             sourceSetNames.zip(dependentConfigurationNames) { a, b -> a + b }
                 .map { names -> names.map { name -> sourceSets[name].allSource } }
