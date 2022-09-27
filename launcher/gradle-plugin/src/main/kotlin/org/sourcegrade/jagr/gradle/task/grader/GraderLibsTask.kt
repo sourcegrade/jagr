35ab1fff9ad0966402d8fae0a6fec3d531e26799
Alexander Staeding <alexanderstaeding@gmail.com>
2022-09-27 23:14:20 +0200

    Fix build libs task

--- launcher/gradle-plugin/src/main/kotlin/org/sourcegrade/jagr/gradle/task/grader/GraderLibsTask.kt
+++ launcher/gradle-plugin/src/main/kotlin/org/sourcegrade/jagr/gradle/task/grader/GraderLibsTask.kt
@@ -1,15 +1,16 @@
 package org.sourcegrade.jagr.gradle.task.grader
 
 import org.gradle.api.Project
+import org.gradle.api.artifacts.ResolvedArtifact
 import org.gradle.api.file.DuplicatesStrategy
+import org.gradle.api.file.FileTree
 import org.gradle.api.tasks.SourceSetContainer
-import org.gradle.api.tasks.TaskAction
 import org.gradle.jvm.tasks.Jar
 import org.gradle.kotlin.dsl.get
 import org.gradle.kotlin.dsl.getByType
 import org.sourcegrade.jagr.gradle.GraderConfiguration
 import org.sourcegrade.jagr.gradle.task.JagrTaskFactory
-import java.util.Locale
+import org.sourcegrade.jagr.launcher.VersionProvider
 
 @Suppress("LeakingThis")
 abstract class GraderLibsTask : Jar(), GraderTask {
@@ -18,35 +19,47 @@ abstract class GraderLibsTask : Jar(), GraderTask {
         group = "build"
         duplicatesStrategy = DuplicatesStrategy.EXCLUDE
         archiveFileName.set(graderName.map { "$it-libs.jar" })
+        from(
+            sourceSetNames.zip(solutionConfigurationName) { names, solution ->
+                createRuntimeDependencyProvider(names, solution).toList()
+            }
+        )
     }
 
-    @TaskAction
-    fun runTask() {
-        // don't include Jagr's runtime dependencies
-        val jagrRuntime = project.configurations["graderPrivateCompileClasspath"]
-            .resolvedConfiguration
-            .firstLevelModuleDependencies
-            .first { it.moduleGroup == "org.sourcegrade" && it.moduleName == "jagr-launcher" }
-            .allModuleArtifacts
-            .map { it.file }
+    private fun createRuntimeDependencyProvider(sourceSets: List<String>, solutionName: String): Sequence<FileTree> = sequence {
+        val sourceSetContainer = project.extensions.getByType<SourceSetContainer>()
+        val jagrArtifacts: Set<ResolvedArtifact> = sourceSets.asSequence()
+            .map { project.configurations[sourceSetContainer[it].runtimeClasspathConfigurationName] }
+            .flatMap { it.resolvedConfiguration.firstLevelModuleDependencies }
+            .first {
+                it.moduleGroup == "org.sourcegrade" &&
+                    it.moduleName == "jagr-launcher" &&
+                    it.moduleVersion == VersionProvider.version
+            }.allModuleArtifacts
 
-        val runtimeDeps = project.extensions
-            .getByType<SourceSetContainer>()
-            .getByName("graderPrivate")
-            .runtimeClasspath.mapNotNull {
-                if (it.path.lowercase(Locale.getDefault()).contains(assignmentId.get()) || jagrRuntime.contains(it)) {
-                    null
-                } else if (it.isDirectory) {
-                    it
-                } else {
-                    project.zipTree(it)
+        (sourceSets.asSequence() + solutionName).flatMap {
+            project.configurations[sourceSetContainer[it].runtimeClasspathConfigurationName].resolvedConfiguration.resolvedArtifacts
+        }.forEach { artifact ->
+            val matchingJagrArtifact: ResolvedArtifact? = jagrArtifacts.firstOrNull { jagrArtifact ->
+                artifact.moduleVersion.id.group == jagrArtifact.moduleVersion.id.group &&
+                    artifact.moduleVersion.id.name == jagrArtifact.moduleVersion.id.name
+            }
+            if (matchingJagrArtifact == null) {
+                yield(project.zipTree(artifact.file))
+            } else {
+                if (artifact.moduleVersion.id.version != matchingJagrArtifact.moduleVersion.id.version) {
+                    logger.error(
+                        "WARNING: Version mismatch for ${artifact.moduleVersion.id.group}:${artifact.moduleVersion.id.name} " +
+                            "(local ${artifact.moduleVersion.id.version} != Jagr's ${matchingJagrArtifact.moduleVersion.id.version}). " +
+                            "This dependency will not be included in the libs jar but will instead be used from Jagr at runtime."
+                    )
                 }
             }
-        from(runtimeDeps)
+        }
     }
 
     internal object Factory : JagrTaskFactory<GraderLibsTask, GraderConfiguration> {
-        override fun determineTaskName(name: String) = "${name}Libs"
+        override fun determineTaskName(name: String) = "${name}BuildLibs"
         override fun configureTask(task: GraderLibsTask, project: Project, configuration: GraderConfiguration) {
             task.description = "Builds the grader libs jar for ${task.sourceSetNames.get()}"
         }
