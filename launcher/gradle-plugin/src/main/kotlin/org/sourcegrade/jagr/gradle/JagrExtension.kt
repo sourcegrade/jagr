b80cb220d44d129fa4d38930d239642919600192
Alexander Staeding <alexanderstaeding@gmail.com>
2022-09-28 13:57:29 +0200

    Clean up JagrExtension

--- launcher/gradle-plugin/src/main/kotlin/org/sourcegrade/jagr/gradle/JagrExtension.kt
+++ launcher/gradle-plugin/src/main/kotlin/org/sourcegrade/jagr/gradle/JagrExtension.kt
@@ -1,18 +1,13 @@
 package org.sourcegrade.jagr.gradle
 
 import org.gradle.api.NamedDomainObjectContainer
-import org.gradle.api.Project
 import org.gradle.api.plugins.ExtensionAware
 import org.gradle.api.provider.Property
-import org.gradle.kotlin.dsl.container
-import javax.inject.Inject
 
-abstract class JagrExtension @Inject constructor(
-    project: Project,
-) : ExtensionAware {
+abstract class JagrExtension : ExtensionAware {
     abstract val assignmentId: Property<String>
-    val graders: NamedDomainObjectContainer<GraderConfiguration> = project.container()
-    val submissions: NamedDomainObjectContainer<SubmissionConfiguration> = project.container()
+    abstract val graders: NamedDomainObjectContainer<GraderConfiguration>
+    abstract val submissions: NamedDomainObjectContainer<SubmissionConfiguration>
     fun graders(configure: NamedDomainObjectContainer<GraderConfiguration>.() -> Unit) = graders.configure()
     fun submissions(configure: NamedDomainObjectContainer<SubmissionConfiguration>.() -> Unit) = submissions.configure()
 }
