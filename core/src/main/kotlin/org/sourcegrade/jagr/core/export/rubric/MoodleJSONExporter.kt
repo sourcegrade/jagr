210773c7d57f04a6e90d9f3c1a7f3246feb9f81a
Dustin Glaser <dustin.glaser@posteo.de>
2022-10-09 10:12:27 +0200

    use HTML exporter in JSON exporter

--- core/src/main/kotlin/org/sourcegrade/jagr/core/export/rubric/MoodleJSONExporter.kt
+++ core/src/main/kotlin/org/sourcegrade/jagr/core/export/rubric/MoodleJSONExporter.kt
@@ -23,10 +23,7 @@ import com.google.inject.Inject
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
-import org.apache.logging.log4j.Logger
-import org.sourcegrade.jagr.api.rubric.GradedCriterion
 import org.sourcegrade.jagr.api.rubric.GradedRubric
-import org.sourcegrade.jagr.api.rubric.PointRange
 import org.sourcegrade.jagr.core.testing.JavaSubmission
 import org.sourcegrade.jagr.launcher.io.GradedRubricExporter
 import org.sourcegrade.jagr.launcher.io.Resource
@@ -34,107 +31,20 @@ import org.sourcegrade.jagr.launcher.io.SubmissionInfo
 import org.sourcegrade.jagr.launcher.io.buildResource
 
 class MoodleJSONExporter @Inject constructor(
-    private val logger: Logger,
+    private val exporterHTML: GradedRubricExporter.HTML
 ) : GradedRubricExporter.Moodle {
+
     override fun export(gradedRubric: GradedRubric): Resource {
         val json = MoodleJSON(
             (gradedRubric.testCycle.submission as JavaSubmission).submissionInfo,
             gradedRubric.grade.minPoints,
-            StringBuilder().writeTable(gradedRubric).toString(),
+            exporterHTML.export(gradedRubric).getInputStream().bufferedReader().readText()
         )
         val jsonString = Json.encodeToString(json)
         return buildResource {
             name = "${gradedRubric.testCycle.submission.info}.json"
             outputStream.bufferedWriter().use { it.write(jsonString) }
         }
-    }
-
-    private fun StringBuilder.writeTable(gradedRubric: GradedRubric): StringBuilder {
-        // Open table
-        append("<table border='1' cellpadding='5' cellspacing='0'>")
-        append("<tbody>")
-
-        // Title
-        append("<tr>")
-        append("<td><strong>${gradedRubric.rubric.title.escaped()}</strong></td>")
-        append("<td></td>")
-        append("<td></td>")
-        append("<td></td>")
-        append("</tr>")
-        appendEmptyHTMLTableRow()
-
-        // Headings
-        append("<tr>")
-        append("<td><strong>Kriterium</strong></td>")
-        append("<td><strong>Möglich</strong></td>")
-        append("<td><strong>Erzielt</strong></td>")
-        append("<td><strong>Kommentar</strong></td>")
-        append("</tr>")
-        appendEmptyHTMLTableRow()
-
-        // Child criteria
-        for (gradedCriterion in gradedRubric.childCriteria) {
-            appendCriterion(gradedCriterion)
-            appendEmptyHTMLTableRow()
-        }
-
-        val rubric = gradedRubric.rubric
-        val grade = gradedRubric.grade
-        val comments = grade.comments
-
-        append("<tr>")
-        append("<td><strong>Gesamt:</strong></td>")
-        append("<td>${rubric.maxPoints}</td>")
-        append("<td>${PointRange.toString(grade)}</td>")
-        append("<td>${comments.firstOrNull()?.escaped() ?: ""}</td>")
-        append("</tr>")
-
-        for (i in 1 until comments.size) {
-            append("<tr>")
-            append("<td></td>")
-            append("<td></td>")
-            append("<td></td>")
-            append("<td>${comments[i].escaped()}</td>")
-            append("</tr>")
-        }
-
-        // Close table
-        append("</tbody>")
-        append("</table>")
-        return this
-    }
-
-    private fun StringBuilder.appendCriterion(gradedCriterion: GradedCriterion): StringBuilder {
-        val criterion = gradedCriterion.criterion
-        val grade = gradedCriterion.grade
-        val comments = grade.comments.joinToString("<br />") { it.escaped() }
-        append("<tr>")
-        append("<td>${criterion.shortDescription.escaped()}</td>")
-        append("<td>${PointRange.toString(criterion)}</td>")
-        append("<td>${PointRange.toString(grade)}</td>")
-        append("<td>$comments</td>")
-        append("</tr>")
-        if (gradedCriterion.childCriteria.isNotEmpty()) {
-            for (childGradedCriterion in gradedCriterion.childCriteria) {
-                appendCriterion(childGradedCriterion)
-            }
-            appendEmptyHTMLTableRow()
-        }
-        return this
-    }
-
-    private fun StringBuilder.appendEmptyHTMLTableRow() {
-        append("<tr>")
-        for (i in 0 until 4) {
-            append("<td></td>")
-        }
-        append("</tr>")
-    }
-
-    private fun String.escaped(): String {
-        return replace("<", "&lt;")
-            .replace(">", "&gt;")
-            .replace("\n", "<br>")
     }
 
     @Serializable
