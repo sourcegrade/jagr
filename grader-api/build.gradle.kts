1bb5c2f834a5607f214d59792e35f32a6c2b5769
Alexander Städing <alexanderstaeding@gmail.com>
2022-10-17 20:40:39 +0200

    Don't publish jagr-grader-api on patch version

--- grader-api/build.gradle.kts
+++ grader-api/build.gradle.kts
@@ -6,6 +6,13 @@ plugins {
 
 apply<JagrPublishPlugin>()
 
+tasks {
+    withType<PublishToMavenRepository> {
+        // check if rootProject version ends with .0 or .0-SNAPSHOT
+        onlyIf { rootProject.version.toString().matches(".*\\.0(-SNAPSHOT)?\$".toRegex()) }
+    }
+}
+
 val apiVersion: String by rootProject
 version = apiVersion
 
