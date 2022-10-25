77ab2313a94c004872f88e1e4d277e7e3d793c18
Alexander Städing <alexanderstaeding@gmail.com>
2022-10-25 09:26:03 +0200

    Revert "Let JUnitTestRefFactoryImpl.Default.get evaluate all matching test results instead of only the first (#143)"
    This reverts commit 36a30f050e88855d44629be0b6df3bebf4e9705d.

--- core/src/main/kotlin/org/sourcegrade/jagr/core/rubric/JUnitTestRefFactoryImpl.kt
+++ core/src/main/kotlin/org/sourcegrade/jagr/core/rubric/JUnitTestRefFactoryImpl.kt
@@ -93,19 +93,12 @@ class JUnitTestRefFactoryImpl @Inject constructor(
         inner class TestNotFoundError : AssertionFailedError("Test result not found")
 
         override operator fun get(testResults: Map<TestIdentifier, TestExecutionResult>): TestExecutionResult {
-            val applicableTestResults: List<TestExecutionResult> = testResults
-                .filter { (identifier, _) -> testSource == identifier.source.orElse(null) }
-                .map { (_, result) -> result }
-            return if (applicableTestResults.isEmpty()) {
-                TestExecutionResult.failed(TestNotFoundError())
-            } else {
-                val failedTests = applicableTestResults.filter { it.status == TestExecutionResult.Status.FAILED }
-                if (failedTests.isNotEmpty()) {
-                    failedTests.first()
-                } else {
-                    TestExecutionResult.successful()
+            for ((identifier, result) in testResults) {
+                if (testSource == identifier.source.orElse(null)) {
+                    return result
                 }
             }
+            return TestExecutionResult.failed(TestNotFoundError())
         }
     }
 
