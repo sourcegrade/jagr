420005065e32e44250e0802e7631093f05671f4f
Alexander Städing <alexanderstaeding@gmail.com>
2022-10-25 09:28:35 +0200

    Check child tests for failures

--- core/src/main/kotlin/org/sourcegrade/jagr/core/testing/TestStatusListenerImpl.kt
+++ core/src/main/kotlin/org/sourcegrade/jagr/core/testing/TestStatusListenerImpl.kt
@@ -23,19 +23,27 @@ import org.apache.logging.log4j.Logger
 import org.junit.platform.engine.TestExecutionResult
 import org.junit.platform.launcher.TestExecutionListener
 import org.junit.platform.launcher.TestIdentifier
+import org.junit.platform.launcher.TestPlan
+import org.opentest4j.AssertionFailedError
 import org.sourcegrade.jagr.api.testing.TestStatusListener
 import org.sourcegrade.jagr.launcher.io.SubmissionInfo
 import java.util.Collections
 
-class TestStatusListenerImpl(
+internal class TestStatusListenerImpl(
     private val logger: Logger,
 ) : TestExecutionListener, TestStatusListener {
 
     private val testResults: MutableMap<TestIdentifier, TestExecutionResult> = mutableMapOf()
     private val linkageErrors: MutableSet<Pair<String?, String>> = LinkedHashSet()
+    private lateinit var testPlan: TestPlan
+
+    override fun testPlanExecutionStarted(testPlan: TestPlan) {
+        this.testPlan = testPlan
+        testResults.clear()
+    }
 
     override fun executionFinished(testIdentifier: TestIdentifier, testExecutionResult: TestExecutionResult) {
-        testResults[testIdentifier] = testExecutionResult
+        testResults[testIdentifier] = aggregate(testIdentifier, testExecutionResult)
         testExecutionResult.throwable.orElse(null)?.also { throwable ->
             if (throwable is LinkageError) {
                 linkageErrors.add(throwable::class.simpleName to throwable.message + " @ " + throwable.stackTrace.firstOrNull())
@@ -43,11 +51,33 @@ class TestStatusListenerImpl(
         }
     }
 
-    fun logLinkageErrors(info: SubmissionInfo) {
+    private fun aggregate(testIdentifier: TestIdentifier, parentResult: TestExecutionResult): TestExecutionResult {
+        if (parentResult.status == TestExecutionResult.Status.FAILED) {
+            return parentResult
+        }
+        val failedChildren: Map<TestIdentifier, TestExecutionResult> = testPlan.getDescendants(testIdentifier).asSequence()
+            .map { it to testResults[it] }
+            .filter { (_, result) -> result != null && result.status != TestExecutionResult.Status.SUCCESSFUL }
+            .map { (identifier, result) -> identifier to result!! }
+            .toMap()
+        if (failedChildren.isEmpty()) {
+            return parentResult
+        }
+        return TestExecutionResult.failed(ContainerFailedError(failedChildren))
+    }
+
+    internal fun logLinkageErrors(info: SubmissionInfo) {
         for (errorMessage in linkageErrors) {
             logger.error("Linkage error @ $info :: $errorMessage")
         }
     }
 
     override fun getTestResults(): Map<TestIdentifier, TestExecutionResult> = Collections.unmodifiableMap(testResults)
+
+    private class ContainerFailedError(
+        failedChildren: Map<TestIdentifier, TestExecutionResult>,
+    ) : AssertionFailedError(
+        "There were failing tests in this container:\n" +
+            failedChildren.entries.joinToString("\n") { (identifier, result) -> "$identifier: $result" }
+    )
 }
