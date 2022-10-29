749a2da21bcdc84145d090f7494bd6f191e6e3a9
Dustin Glaser <dustin.glaser@posteo.de>
2022-10-29 23:12:33 +0200

    remove title from html

--- core/src/main/kotlin/org/sourcegrade/jagr/core/export/rubric/BasicHTMLExporter.kt
+++ core/src/main/kotlin/org/sourcegrade/jagr/core/export/rubric/BasicHTMLExporter.kt
@@ -17,7 +17,7 @@ class BasicHTMLExporter : GradedRubricExporter.HTML {
 
     override fun export(gradedRubric: GradedRubric): Resource {
         val builder = StringBuilder()
-        builder.pageStart("Grading Result")
+        builder.pageStart()
         builder.table(gradedRubric)
         builder.pageEnd()
         return buildResource {
@@ -54,12 +54,11 @@ class BasicHTMLExporter : GradedRubricExporter.HTML {
         tableEnd()
     }
 
-    private fun StringBuilder.pageStart(title: String) {
+    private fun StringBuilder.pageStart() {
         append("<html>")
         append("<head>")
         append("""<meta charset="utf-8">""")
         append("""<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">""")
-        append("<title>${title.escaped()}</title>")
         append("</head>")
         append("<body>")
         append("""<div class="container">""")
