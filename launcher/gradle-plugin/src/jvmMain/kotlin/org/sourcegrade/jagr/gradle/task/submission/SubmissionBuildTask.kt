a38fee022dc5b2ff546a6d69169848138bb37108
Alexander Städing <alexanderstaeding@gmail.com>
2023-06-09 20:27:51 +0200

    Start separating modules in submission artifact

--- launcher/gradle-plugin/src/jvmMain/kotlin/org/sourcegrade/jagr/gradle/task/submission/SubmissionBuildTask.kt
+++ launcher/gradle-plugin/src/jvmMain/kotlin/org/sourcegrade/jagr/gradle/task/submission/SubmissionBuildTask.kt
@@ -1,10 +1,28 @@
+/*
+ *   Jagr - SourceGrade.org
+ *   Copyright (C) 2021-2023 Alexander Städing
+ *   Copyright (C) 2021-2023 Contributors
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package org.sourcegrade.jagr.gradle.task.submission
 
 import org.gradle.api.Project
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.InputFile
 import org.gradle.jvm.tasks.Jar
-import org.gradle.kotlin.dsl.get
 import org.gradle.kotlin.dsl.property
 import org.sourcegrade.jagr.gradle.extension.SubmissionConfiguration
 import org.sourcegrade.jagr.gradle.extension.getSourceSet
@@ -22,7 +40,19 @@ abstract class SubmissionBuildTask : Jar(), SubmissionTask {
         group = "build"
         dependsOn(configurationName.map(SubmissionWriteInfoTask.Factory::determineTaskName))
         from(submissionInfoFile)
-        from(sourceSetNames.map { all -> all.map { it.getSourceSet(project).allSource } })
+
+        project.subprojects.forEach { subproject ->
+            from(
+                sourceSetNames.map { all ->
+                    all
+                        .filter { it.projectPath == subproject.path }
+                        .map { it.getSourceSet(project).allSource }
+                },
+            ) { copy ->
+                copy.into(subproject.path)
+            }
+        }
+
         archiveFileName.set(
             assignmentId.zip(studentId) { assignmentId, studentId ->
                 "$assignmentId-$studentId"
