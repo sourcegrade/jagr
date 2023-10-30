sourcegrade/jagr

commit 4ab7a7f9c6ef9415dc0617819e5b9cf55e53bae9

Fix project.buildDir deprecations and clean up file properties in tasks

launcher/gradle-plugin/src/main/kotlin/org/sourcegrade/jagr/gradle/extension/RelativeFiles.kt
@@ -1,0 +1,29 @@
+package org.sourcegrade.jagr.gradle.extension
+
+import org.gradle.api.Project
+import org.gradle.api.provider.Property
+import org.gradle.api.provider.Provider
+import org.gradle.kotlin.dsl.property
+import org.sourcegrade.jagr.gradle.task.TargetSourceSetsTask
+import java.io.File
+
+fun Project.relative(path: String): Project {
+    if (path.isEmpty()) return this
+    return project.project(path)
+}
+
+fun TargetSourceSetsTask.resolveInBuildDirectory(pathResolver: (configurationName: String) -> String): Provider<File> {
+    return configurationName.flatMap { configurationName ->
+        project.layout.buildDirectory.map { it.file(pathResolver(configurationName)).asFile }
+    }
+}
+
+fun TargetSourceSetsTask.createSubmissionInfoFileProperty(): Property<File> {
+    return project.objects.property<File>()
+        .value(resolveInBuildDirectory { "resources/jagr/$it/submission-info.json" })
+}
+
+fun TargetSourceSetsTask.createGraderInfoFileProperty(): Property<File> {
+    return project.objects.property<File>()
+        .value(resolveInBuildDirectory { "resources/jagr/$it/grader-info.json" })
+}

launcher/gradle-plugin/src/main/kotlin/org/sourcegrade/jagr/gradle/task/grader/GraderRunTask.kt
@@ -18,6 +18,8 @@ import org.sourcegrade.jagr.gradle.extension.GraderConfiguration
 import org.sourcegrade.jagr.gradle.extension.JagrDownloadExtension
 import org.sourcegrade.jagr.gradle.extension.JagrExtension
 import org.sourcegrade.jagr.gradle.extension.ProjectSourceSetTuple
+import org.sourcegrade.jagr.gradle.extension.createGraderInfoFileProperty
+import org.sourcegrade.jagr.gradle.extension.createSubmissionInfoFileProperty
 import org.sourcegrade.jagr.gradle.extension.relative
 import org.sourcegrade.jagr.gradle.task.JagrDownloadTask
 import org.sourcegrade.jagr.gradle.task.JagrTaskFactory
@@ -51,12 +53,10 @@ import java.nio.file.Path
 abstract class GraderRunTask : DefaultTask(), GraderTask {
 
     @get:InputFile
-    val graderInfoFile: Property<File> = project.objects.property<File>()
-        .value(configurationName.map { project.buildDir.resolve("resources/jagr/$it/grader-info.json") })
+    val graderInfoFile: Property<File> = createGraderInfoFileProperty()
 
     @get:InputFile
-    val submissionInfoFile: Property<File> = project.objects.property<File>()
-        .value(submissionConfigurationName.map { project.buildDir.resolve("resources/jagr/$it/submission-info.json") })
+    val submissionInfoFile: Property<File> = createSubmissionInfoFileProperty()
 
     @get:Input
     val jagrJar: Property<Path> = project.objects.property()
@@ -100,7 +100,7 @@ abstract class GraderRunTask : DefaultTask(), GraderTask {
         }.create(jagr)
         val collector = emptyCollector(jagr)
         // TODO: Properly configure task output
-        val rubricOutputDir = project.buildDir.resolve("resources/jagr/${configurationName.get()}/rubrics/")
+        val rubricOutputDir = project.buildFile.resolve("resources/jagr/${configurationName.get()}/rubrics/")
         val rubrics = mutableMapOf<String, Boolean>()
         collector.setListener { result ->
             result.rubrics.keys.forEach {

launcher/gradle-plugin/src/main/kotlin/org/sourcegrade/jagr/gradle/task/submission/SubmissionBuildTask.kt
@@ -4,9 +4,8 @@ import org.gradle.api.Project
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.InputFile
 import org.gradle.jvm.tasks.Jar
-import org.gradle.kotlin.dsl.get
-import org.gradle.kotlin.dsl.property
 import org.sourcegrade.jagr.gradle.extension.SubmissionConfiguration
+import org.sourcegrade.jagr.gradle.extension.createSubmissionInfoFileProperty
 import org.sourcegrade.jagr.gradle.extension.getSourceSet
 import org.sourcegrade.jagr.gradle.task.JagrTaskFactory
 import java.io.File
@@ -15,8 +14,7 @@ import java.io.File
 abstract class SubmissionBuildTask : Jar(), SubmissionTask {
 
     @get:InputFile
-    val submissionInfoFile: Property<File> = project.objects.property<File>()
-        .value(configurationName.map { project.buildDir.resolve("resources/jagr/$it/submission-info.json") })
+    val submissionInfoFile: Property<File> = createSubmissionInfoFileProperty()
 
     init {
         group = "build"

launcher/gradle-plugin/src/main/kotlin/org/sourcegrade/jagr/gradle/task/submission/SubmissionWriteInfoTask.kt
@@ -12,9 +12,9 @@ import org.gradle.api.tasks.TaskAction
 import org.gradle.kotlin.dsl.get
 import org.gradle.kotlin.dsl.getByType
 import org.gradle.kotlin.dsl.mapProperty
-import org.gradle.kotlin.dsl.property
 import org.sourcegrade.jagr.gradle.extension.JagrExtension
 import org.sourcegrade.jagr.gradle.extension.SubmissionConfiguration
+import org.sourcegrade.jagr.gradle.extension.createSubmissionInfoFileProperty
 import org.sourcegrade.jagr.gradle.mergeSourceSets
 import org.sourcegrade.jagr.gradle.task.JagrTaskFactory
 import org.sourcegrade.jagr.gradle.task.WriteInfoTask
@@ -38,8 +38,7 @@ abstract class SubmissionWriteInfoTask : WriteInfoTask(), SubmissionTask {
         .value(configurationName.map { c -> primaryContainer[c].getAllDependencies() })
 
     @get:OutputFile
-    val submissionInfoFile: Property<File> = project.objects.property<File>()
-        .value(configurationName.map { project.buildDir.resolve("resources/jagr/$it/submission-info.json") })
+    val submissionInfoFile: Property<File> = createSubmissionInfoFileProperty()
 
     init {
         group = "jagr resources"

launcher/gradle-plugin/src/main/kotlin/org/sourcegrade/jagr/gradle/extension/RelativeProjectSearch.kt
@@ -1,8 +1,0 @@
-package org.sourcegrade.jagr.gradle.extension
-
-import org.gradle.api.Project
-
-fun Project.relative(path: String): Project {
-    if (path.isEmpty()) return this
-    return project.project(path)
-}

launcher/gradle-plugin/src/main/kotlin/org/sourcegrade/jagr/gradle/task/grader/GraderBuildTask.kt
@@ -8,9 +8,9 @@ import org.gradle.api.tasks.InputFile
 import org.gradle.jvm.tasks.Jar
 import org.gradle.kotlin.dsl.get
 import org.gradle.kotlin.dsl.getByType
-import org.gradle.kotlin.dsl.property
 import org.sourcegrade.jagr.gradle.extension.GraderConfiguration
 import org.sourcegrade.jagr.gradle.extension.JagrExtension
+import org.sourcegrade.jagr.gradle.extension.createGraderInfoFileProperty
 import org.sourcegrade.jagr.gradle.task.JagrTaskFactory
 import java.io.File
 
@@ -18,8 +18,7 @@ import java.io.File
 abstract class GraderBuildTask : Jar(), GraderTask {
 
     @get:InputFile
-    val graderInfoFile: Property<File> = project.objects.property<File>()
-        .value(configurationName.map { project.buildDir.resolve("resources/jagr/$it/grader-info.json") })
+    val graderInfoFile: Property<File> = createGraderInfoFileProperty()
 
     init {
         group = "build"

launcher/gradle-plugin/src/main/kotlin/org/sourcegrade/jagr/gradle/task/grader/GraderWriteInfoTask.kt
@@ -13,9 +13,9 @@ import org.gradle.api.tasks.TaskAction
 import org.gradle.kotlin.dsl.get
 import org.gradle.kotlin.dsl.getByType
 import org.gradle.kotlin.dsl.mapProperty
-import org.gradle.kotlin.dsl.property
 import org.sourcegrade.jagr.gradle.extension.GraderConfiguration
 import org.sourcegrade.jagr.gradle.extension.JagrExtension
+import org.sourcegrade.jagr.gradle.extension.createGraderInfoFileProperty
 import org.sourcegrade.jagr.gradle.forEachFile
 import org.sourcegrade.jagr.gradle.mergeSourceSets
 import org.sourcegrade.jagr.gradle.task.JagrTaskFactory
@@ -49,8 +49,7 @@ abstract class GraderWriteInfoTask : WriteInfoTask(), GraderTask {
         .value(configurationName.map { c -> primaryContainer[c].getAllDependenciesRecursive() })
 
     @get:OutputFile
-    val graderInfoFile: Property<File> = project.objects.property<File>()
-        .value(configurationName.map { project.buildDir.resolve("resources/jagr/$it/grader-info.json") })
+    val graderInfoFile: Property<File> = createGraderInfoFileProperty()
 
     init {
         group = "jagr resources"

commit 6f1793cd0c1f84122a9a5bae55f59e00179166cb

Fix grader using incorrect configuration name for solution introduced in 4ab7a7f9c6ef9415dc0617819e5b9cf55e53bae9

launcher/gradle-plugin/src/main/kotlin/org/sourcegrade/jagr/gradle/extension/RelativeFiles.kt
@@ -12,18 +12,23 @@ fun Project.relative(path: String): Project {
     return project.project(path)
 }
 
-fun TargetSourceSetsTask.resolveInBuildDirectory(pathResolver: (configurationName: String) -> String): Provider<File> {
-    return configurationName.flatMap { configurationName ->
+fun TargetSourceSetsTask.resolveInBuildDirectory(
+    configurationNameProvider: Provider<String>,
+    pathResolver: (configurationName: String) -> String,
+): Provider<File> {
+    return configurationNameProvider.flatMap { configurationName ->
         project.layout.buildDirectory.map { it.file(pathResolver(configurationName)).asFile }
     }
 }
 
-fun TargetSourceSetsTask.createSubmissionInfoFileProperty(): Property<File> {
+fun TargetSourceSetsTask.createSubmissionInfoFileProperty(
+    configurationNameProvider: Provider<String>,
+): Property<File> {
     return project.objects.property<File>()
-        .value(resolveInBuildDirectory { "resources/jagr/$it/submission-info.json" })
+        .value(resolveInBuildDirectory(configurationNameProvider) { "resources/jagr/$it/submission-info.json" })
 }
 
 fun TargetSourceSetsTask.createGraderInfoFileProperty(): Property<File> {
     return project.objects.property<File>()
-        .value(resolveInBuildDirectory { "resources/jagr/$it/grader-info.json" })
+        .value(resolveInBuildDirectory(configurationName) { "resources/jagr/$it/grader-info.json" })
 }

launcher/gradle-plugin/src/main/kotlin/org/sourcegrade/jagr/gradle/task/grader/GraderRunTask.kt
@@ -56,7 +56,7 @@ abstract class GraderRunTask : DefaultTask(), GraderTask {
     val graderInfoFile: Property<File> = createGraderInfoFileProperty()
 
     @get:InputFile
-    val submissionInfoFile: Property<File> = createSubmissionInfoFileProperty()
+    val submissionInfoFile: Property<File> = createSubmissionInfoFileProperty(submissionConfigurationName)
 
     @get:Input
     val jagrJar: Property<Path> = project.objects.property()

launcher/gradle-plugin/src/main/kotlin/org/sourcegrade/jagr/gradle/task/submission/SubmissionBuildTask.kt
@@ -14,7 +14,7 @@ import java.io.File
 abstract class SubmissionBuildTask : Jar(), SubmissionTask {
 
     @get:InputFile
-    val submissionInfoFile: Property<File> = createSubmissionInfoFileProperty()
+    val submissionInfoFile: Property<File> = createSubmissionInfoFileProperty(configurationName)
 
     init {
         group = "build"

launcher/gradle-plugin/src/main/kotlin/org/sourcegrade/jagr/gradle/task/submission/SubmissionWriteInfoTask.kt
@@ -38,7 +38,7 @@ abstract class SubmissionWriteInfoTask : WriteInfoTask(), SubmissionTask {
         .value(configurationName.map { c -> primaryContainer[c].getAllDependencies() })
 
     @get:OutputFile
-    val submissionInfoFile: Property<File> = createSubmissionInfoFileProperty()
+    val submissionInfoFile: Property<File> = createSubmissionInfoFileProperty(configurationName)
 
     init {
         group = "jagr resources"