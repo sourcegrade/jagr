c97fa71eb5c4394a2bb0b5f49d4f70a0db1b9a5a
Alexander Städing <alexanderstaeding@gmail.com>
2023-10-30 14:52:31 +0100

    Revert accidental changes in BatchCompilation

--- core/src/main/kotlin/org/sourcegrade/jagr/core/executor/BatchCompilation.kt
+++ core/src/main/kotlin/org/sourcegrade/jagr/core/executor/BatchCompilation.kt
@@ -76,10 +76,9 @@ class CompiledBatchFactoryImpl @Inject constructor(
         // maps assignment ids to files that should be replaced with solution files
         val replacements = submissionFileOverrides.mapValues { (assignmentId, solutionOverrides) ->
             val gradersForAssignment = graders.filter { it.info.assignmentId == assignmentId }
-            val t: Map<String, JavaSourceFile> = solutionOverrides.mapNotNull { solutionOverride ->
+            solutionOverrides.mapNotNull { solutionOverride ->
                 gradersForAssignment.firstNotNullOfOrNull { it.container.source.sourceFiles[solutionOverride] }
             }.associateBy { it.fileName }
-            t
         }
 
         val submissions: List<Submission> = batch.submissions.compile(
