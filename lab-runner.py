d5ba1836d6ccf2d3a72c334f4ff2cf41e6e116fe
Rdeisenroth <rdeisenroth@gmail.com>
2024-08-29 01:19:31 +0200

    api changes

--- lab-runner.py
+++ lab-runner.py
@@ -2,6 +2,7 @@
 import json
 import os
 import shutil
+from enum import Enum
 
 import requests
 from colorama import Style
@@ -22,17 +23,25 @@ if operator_base_url[-1] == '/':
     operator_base_url = operator_base_url[:-1]
 
 
-# def set_job_status(job_id, status):
-#     response = requests.post(f'{operator_base_url}/jobs/updateStatus?id={job_id}&key={runner_key}&status={status}')
-#     # check if response is valid
-#     if response.status_code != 200:
-#         print(f'Error updating job status: {response.text}')
-#         return
-#     print(f'Updated job {job_id} status to {status}')
-#     return response.json()
+class JobStatus(Enum):
+    PENDING = 'PENDING'
+    RUNNING = 'RUNNING'
+    COMPLETED = 'COMPLETED'
+    FAILED = 'FAILED'
+
+
+class RunnerStatus(Enum):
+    IDLE = 'IDLE'
+    STARTING = 'STARTING'
+    RUNNING = 'RUNNING'
+    UPLOADING = 'UPLOADING'
+    CLEANUP = 'CLEANUP'
+    STOPPING = 'STOPPING'
+    DONE = 'DONE'
+
 
 def send_results(job_id, grade, rubric, failed=False, logs=None):
-    set_runner_status('uploading')
+    set_runner_status(RunnerStatus.UPLOADING)
     payload = {"grade": grade, "result": rubric}
     if failed:
         payload["status"] = "failed"
@@ -44,12 +53,12 @@ def send_results(job_id, grade, rubric, failed=False, logs=None):
         print(f'Error sending results: {response.text}')
         return
     print(f'Sent results for job {job_id}')
-    set_runner_status('stopping')
+    set_runner_status(RunnerStatus.STOPPING)
     return response.json()
 
 
-def set_runner_status(status):
-    response = requests.post(f'{operator_base_url}/runners/updateStatus?status={status}&key={runner_key}')
+def set_runner_status(status: RunnerStatus):
+    response = requests.post(f'{operator_base_url}/runners/updateStatus?status={status.value}&key={runner_key}')
     # check if response is valid
     if response.status_code != 200:
         print(f'Error updating runner status: {response.text}')
@@ -58,38 +67,35 @@ def set_runner_status(status):
     return response.json()
 
 
-# get all jobs from operator
-def get_jobs():
-    print('Fetching available jobs...')
-    response = requests.get(f'{operator_base_url}/jobs?pendingOnly=true&key={runner_key}')
+# get next job from operator
+def get_next_job():
+    print('Fetching next job...')
+    response = requests.post(f'{operator_base_url}/jobs/claim?key={runner_key}')
     # check if response is valid
     if response.status_code != 200:
-        print(f'Error getting jobs: {response.text}')
-        return {}
+        print(f'Error getting next job: {response.text}')
+        return None
     response_json = response.json()
-    if 'jobs' not in response.json():
-        print('Invalid response from operator, "jobs" key not found')
-        return {}
-    jobs = response_json['jobs']
-    print(f'Found {len(jobs)} total jobs')
-    return jobs
+    job = response_json
+    print(f'Found next job: {job["id"]} ({job["name"]})')
+    return job
 
 
-# claim a job
-def claim_job(job):
-    job_id = job['id']
-    job_name = job['name']
-    print(f'Claiming job {job_id} ({job_name})')
-    response = requests.post(f'{operator_base_url}/jobs/claim?id={job_id}&key={runner_key}')
-    # check if response is valid
-    if response.status_code != 200:
-        print(f'Error claiming job: {response.text}')
-        return
-    if response.json()['id'] != job_id:
-        print(f'Error claiming job: job id mismatch')
-        return
-    print(f'Successfully claimed job {job_id} ({job_name})')
-    return response.json()
+# # claim a job
+# def claim_job(job):
+#     job_id = job['id']
+#     job_name = job['name']
+#     print(f'Claiming job {job_id} ({job_name})')
+#     response = requests.post(f'{operator_base_url}/jobs/claim?id={job_id}&key={runner_key}')
+#     # check if response is valid
+#     if response.status_code != 200:
+#         print(f'Error claiming job: {response.text}')
+#         return
+#     if response.json()['id'] != job_id:
+#         print(f'Error claiming job: job id mismatch')
+#         return
+#     print(f'Successfully claimed job {job_id} ({job_name})')
+#     return response.json()
 
 
 def prepare_job(job):
@@ -132,7 +138,7 @@ def run_job(job):
     print('Running the job...')
     # set the job status to running
     job_id = job['id']
-    set_runner_status('running')
+    set_runner_status(RunnerStatus.RUNNING)
     print(f'Updated job {job_id} status to running')
     # run the tests
     os.system('jagr --no-export')
@@ -148,16 +154,15 @@ def run_job(job):
             break
     if rubrics_file_name is None:
         print('Results file not found')
-        set_runner_status('failed')
+        set_runner_status(RunnerStatus.DONE)
         exit(1)
     # read the results from the rubrics/lab/*.json file
-    results = None
     with open(f'rubrics/lab/{rubrics_file_name}', 'r') as file:
         results = json.load(file)
     # check for totalPointsMin and totalPointsMax in the results
     if 'totalPointsMin' not in results or 'totalPointsMax' not in results:
         print('totalPointsMin or totalPointsMax not found in results')
-        set_runner_status('uploading')
+        set_runner_status(RunnerStatus.UPLOADING)
         send_results(job_id, 0, results, True)
         exit(1)
     total_points_min = results['totalPointsMin']
@@ -171,25 +176,12 @@ def run_job(job):
 
 
 def main():
-    jobs = get_jobs()
-    if len(jobs) == 0:
-        print('No jobs available')
-        return
-
-    # find jobs that have "base_image" set to "jagr-lab-runner:latest"
-    legible_jobs = [job for job in jobs if
-                    job['baseImage'] == 'jagr-lab-runner:latest'
-                    and job['allocatedMemory'] <= max_memory
-                    and job['allocatedCpuCores'] <= max_cpu_cores
-                    ]
-    if len(legible_jobs) == 0:
-        print('No legible jobs available')
-        return
-    print(f'Found {len(legible_jobs)} legible jobs')
-
     # claim the first job
-    job = claim_job(legible_jobs[0])
-    set_runner_status('running')
+    job = get_next_job()
+    if job is None:
+        print('No job available')
+        return
+    set_runner_status(RunnerStatus.STARTING)
 
     # prepare the job
     prepare_job(job)
@@ -198,7 +190,7 @@ def main():
     run_job(job)
 
     # stop the container
-    set_runner_status('done')
+    set_runner_status(RunnerStatus.DONE)
     exit(0)
 
 
