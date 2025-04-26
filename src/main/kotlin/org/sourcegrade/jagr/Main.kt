9766ec507e53ae939da46e291a1ecc4b0b89706b
Alexander Städing <alex@staeding.com>
2025-04-26 15:04:06 +0200

    Fix compilation error

--- src/main/kotlin/org/sourcegrade/jagr/Main.kt
+++ src/main/kotlin/org/sourcegrade/jagr/Main.kt
@@ -20,6 +20,7 @@
 package org.sourcegrade.jagr
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.main
 import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.help
 import com.github.ajalt.clikt.parameters.options.option
